f67d601558cfb898cd5b67974189ee9dff87cbc7
Mick Cooney <mickcooney@gmail.com>
2018-06-19 22:01:32 +0100

    Renaming the files and blanking the numbers to decide ordering later

--- load_data_script.R
+++ load_data_script.R
@@ -9,3 +9,12 @@ claims_transactions_tbl <- data_frame(file_src = data_files) %>%
     bind_rows() %>%
     unnest() %>%
     select(-file_src)
+
+
+claims_snapshot_tbl <- claims_transactions_tbl %>%
+    group_by(country_code, claim_id, claim_type) %>%
+    top_n(1, wt = transaction_date) %>%
+    ungroup() %>%
+    mutate(claim_lifetime = as.numeric(transaction_date - incident_date)
+          ,yearmonth      = format(incident_date, '%Y%m')
+           )
