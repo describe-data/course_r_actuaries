3b13f4afe408cec1dd6de4be3897ab710cf56cde
Mick Cooney <mickcooney@gmail.com>
2018-06-21 14:15:16 +0100

    Wrote up content for the data visualisation lesson

--- load_data_script.R
+++ load_data_script.R
@@ -1,4 +1,6 @@
 library(tidyverse)
+library(feather)
+library(scales)
 
 
 # Load all claims transactions data
@@ -8,13 +10,16 @@ claims_transactions_tbl <- data_frame(file_src = data_files) %>%
     mutate(data = map(file_src, read_csv, col_types = cols())) %>%
     bind_rows() %>%
     unnest() %>%
-    select(-file_src)
+    select(-file_src) %>%
+    mutate(claim_lifetime = as.numeric(transaction_date - incident_date)
+          ,yearmonth      = format(incident_date, '%Y%m')
+    )
 
 
 claims_snapshot_tbl <- claims_transactions_tbl %>%
     group_by(country_code, claim_id, claim_type) %>%
     top_n(1, wt = transaction_date) %>%
-    ungroup() %>%
-    mutate(claim_lifetime = as.numeric(transaction_date - incident_date)
-          ,yearmonth      = format(incident_date, '%Y%m')
-           )
+    ungroup()
+
+
+claim_triangles_tbl <- read_feather('data/yearly_triangles.feather')
