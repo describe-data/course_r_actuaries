adb482e4a790ce691bbe5331727b507aff3fa896
Mick Cooney <mickcooney@gmail.com>
2019-02-06 10:52:44 +0000

    Cleaned up some of the code - want to add more to the data loading piece

--- load_data_script.R
+++ load_data_script.R
@@ -6,7 +6,7 @@ library(scales)
 # Load all claims transactions data
 data_files <- list.files('data', full.names = TRUE, pattern = '\\.csv')
 
-claim_transactions_tbl <- data_frame(file_src = data_files) %>%
+claim_transactions_tbl <- tibble(file_src = data_files) %>%
     mutate(data = map(file_src, read_csv, col_types = cols())) %>%
     bind_rows() %>%
     unnest() %>%
